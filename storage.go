17ed4e0c4922cfe8699f78a66cc1ca1e73b8a0a1
lewdest <lewdest@noreply.codeberg.org>
2026-07-02 20:21:41 +0200

    fix: InMemoryStorage not properly removing finished invoices. thanks ky

--- storage.go
+++ storage.go
@@ -147,7 +147,7 @@ func (i *InMemoryStorage) UpdateInvoices(_ context.Context, invoices []Invoice) 
 		}
 
 		if inv.Paid() {
-			i.activeInvoices[inv.Chain] = slices.Delete(i.activeInvoices[inv.Chain], idx, idx)
+			i.activeInvoices[inv.Chain] = slices.Delete(i.activeInvoices[inv.Chain], idx, idx+1)
 			return nil
 		}
 
